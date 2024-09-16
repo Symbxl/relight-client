677a7d9843278bc75239f1d28ec0a1ea3010cae1
System Administrator <root@Users-MacBook-Pro.local>
2024-09-15 23:00:46 -0700

    done

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,6 @@ export default function Home() {
           fontFamily='"General Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
         />
         <Gallery images={images} />
-        <Slide />
         <DisplayMobile />
         <Section
           title="Why choose Relight?"
@@ -73,6 +72,7 @@ export default function Home() {
           margin="2rem 0"
         />
         <DisplayCarousel />
+        <Slide />
         <DisplayFAQ />
         <Footer />
       </Container>
